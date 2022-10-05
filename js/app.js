0b65019c067fbd00dff3b2e0d0b6e3c538a822cc
jubayergithubrit <jubayeralmahfuj001@gmail.com>
2022-10-05 13:36:31 +0600

    pin-matcher-solution

---
+++ js/app.js
@@ -0,0 +1,61 @@
+function getPin() {
+    const pin = generatePin();
+    const pinString = pin + '';
+    if (pinString.length === 4) {
+        return pin;
+    }
+    else {
+        console.log('get three digit pin', pin);
+        return getPin();
+    }
+}
+
+function generatePin() {
+    const random = Math.round(Math.random() * 10000);
+    return random;
+}
+document.getElementById('generate-pin').addEventListener('click', function () {
+    const pin = getPin();
+
+    const displayPinField = document.getElementById('display-field');
+    displayPinField.value = pin;
+})
+document.getElementById('calculator').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const typedNumberField = document.getElementById('typed-numbers');
+    const PrevioiusTypedNumber = typedNumberField.value;
+    if (isNaN(number)) {
+        if (number === 'C') {
+            typedNumberField.value = '';
+        }
+        else if (number === '<') {
+            const digits = PrevioiusTypedNumber.split('');
+            digits.pop('');
+            const remainigDigits = digits.join('');
+            typedNumberField.value = remainigDigits;
+        }
+    }
+    else {
+        const newTypedNumber = PrevioiusTypedNumber + number;
+        typedNumberField.value = newTypedNumber;
+    }
+
+})
+document.getElementById('varify-pin').addEventListener('click', function () {
+    const displayPinField = document.getElementById('display-field');
+    const currentPin = displayPinField.value;
+
+    const typedNumberField = document.getElementById('typed-numbers');
+    const typedNumber = typedNumberField.value;
+    const pinSuccessMessage = document.getElementById('pin-success');
+    const pinFailureMessage = document.getElementById('pin-failure');
+    if (typedNumber === currentPin) {
+        pinSuccessMessage.style.display = 'block';
+        pinFailureMessage.style.display = 'none';
+    }
+    else {
+        pinFailureMessage.style.display = 'block';
+        pinSuccessMessage.style.display = 'none';
+    }
+
+})
